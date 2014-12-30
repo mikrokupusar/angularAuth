ffd96a34d7e943b8988b8fc05c673aac5fc333f0
mikrokupusar <ilicigor@gmail.com>
2014-12-30 16:11:09 +0100

    zavrsena prva dva dela

---
+++ AngularJSAuthentication.API/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AngularJSAuthentication.API.Controllers
+{
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        private AuthRepository _repo = null;
+        
+        public AccountController()
+        {
+            _repo = new AuthRepository();
+        }
+
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(UserModel userModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await _repo.RegisterUser(userModel);
+            IHttpActionResult errorResult = GetErrorResult(result);
+
+            if (errorResult != null)
+            {
+                return errorResult;
+            }
+
+            return Ok();
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    //no modelstate errors are available to send, just return empty badrequest
+                    return BadRequest(ModelState);
+                }
+            }
+
+            return null;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AngularJSAuthentication.Web/app/services/ordersService.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+app.factory('ordersService', ['$http', function ($http) {
+    var serviceBase = 'http://localhost:57037/';
+    var ordersServiceFactory = {};
+
+    var _getOrders = function () {
+        return $http.get(serviceBase + 'api/orders').then(function (results) {
+            return results;
+        });
+    };
+
+    ordersServiceFactory.getOrders = _getOrders;
+
+    return ordersServiceFactory;
+}]);
